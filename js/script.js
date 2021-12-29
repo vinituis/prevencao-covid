241aa8dd75bb869db2c3bc447fe298e096459f12
Vinicius Fernandes <vinicius.turu@gmail.com>
2021-12-29 11:06:43 -0300

    header e a primeira seção iniciadas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    });
+
+});
