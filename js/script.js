963d7e9fa1393769fe76520d20156241f6137847
Vinicius Fernandes <vinicius.turu@gmail.com>
2021-12-29 21:24:07 -0300

    seção de sintomas finalizado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 
     $('#menu').click(function() {
+        
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
+    
     });
 
     $(window).on('load scroll',function(){
+    
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+    
+        if($(window).scrollTop() > 0) {
+            $('header').addClass('sticky');
+        }else{
+            $('header').removeClass('sticky');
+        }
     });
 
 });
